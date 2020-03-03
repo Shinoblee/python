f5d11fd9fa530bfe1c55b85401a6ca97e170c881
Lee Simon <shinoblee@gmail.com>
2020-03-03 16:09:28 -0500

    Added modules examples

---
+++ Modules/app.py
@@ -0,0 +1,15 @@
+# from ecommerce.shopping import calc_shipping, calc_tax
+
+# calc_tax()
+
+
+from ecommerce.shopping import sales
+# import Modules.sales
+
+
+# sales.calc_shipping()
+
+# print(dir(sales))
+# print(sales.__name__)
+# print(sales.__package__)
+# print(sales.__file__)
---
+++ Modules/ecommerce/shopping/sales.py
@@ -0,0 +1,19 @@
+# from ecommerce.customer import contact
+# from ..customer import contact
+
+# contact.contact_customer()
+
+# print("Sales initialized", __name__)
+
+
+def calc_tax():
+    pass
+
+
+def calc_shipping():
+    pass
+
+
+if __name__ == "__main__":
+    print("Sales started")
+    calc_tax()
